2c96b83a62c7f7d9f382a7b0b6ae2c8c7b8d1115
Jérémy <frazierjeremi@gmail.com>
2024-06-21 11:44:00 +0200

    init repo

---
+++ app.js
@@ -0,0 +1,5 @@
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Serveur backend démarré sur le port ${port}`);
+  });
---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const db= mysql.createConnection({
+    host: process.env.DB_HOST_DEV,
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log('Erreur lors de la connexion à la base de données : ', err)
+    } else {
+        console.log('Connection effectué')
+    }
+})
